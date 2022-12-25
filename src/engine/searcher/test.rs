1e64961a365fcafc035b39d59ef8e2269f9d4190
Marcin Pieczka <marcin.pieczka@gmail.com>
2022-12-25 12:33:59 +0100

    building of one level of tree

--- src/engine/searcher/test.rs
+++ src/engine/searcher/test.rs
@@ -1,16 +1,16 @@
 use crate::engine::searcher::{MoveNode, Searcher};
-use chess::{Board, ChessMove, Square};
+use chess::{Board, ChessMove, Game, Square};
 
 #[test]
 fn test_building_tree() {
-    let mut searcher = Searcher::default();
-    searcher.build_tree(Board::default());
-    println!("{:?}", searcher.moves.iter().next().unwrap().data());
+    let board = Game::new().current_position();
+    let mut searcher = Searcher::new(&board);
+    println!("{:?}", board.side_to_move());
+    searcher.build_tree();
+    let expected_move = unsafe { ChessMove::new(Square::new(8), Square::new(16), None) };
+    let expected_board = Game::new().current_position();
     assert_eq!(
         searcher.moves.iter().next().unwrap().data(),
-        &MoveNode {
-            chess_move: unsafe { ChessMove::new(Square::new(8), Square::new(16), None) },
-            evaluation: 0.0
-        }
+        &MoveNode::new(&expected_board, Some(expected_move))
     );
 }
