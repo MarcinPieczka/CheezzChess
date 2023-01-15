ff1f95cb2690e435804120e0bd7a7301ea78a7ca
Marcin Pieczka <marcin.pieczka@gmail.com>
2023-01-15 16:59:51 +0100

    LocoChess: go back to vec

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -1,22 +1,31 @@
 use crate::engine::eval::eval;
-use chess::{Board, CacheTable, ChessMove, Color, MoveGen, Piece, Square};
+use chess::{Board, BoardStatus, CacheTable, ChessMove, Color, MoveGen, Piece, Square};
 use log::info;
-use trees::{tr, Node, Tree};
 use vampirc_uci::UciMessage;
 
 pub struct Position {
-    chess_move: ChessMove,
-    potential_next_moves: Option<Vec<ChessMove>>,
+    chess_move: Option<ChessMove>,
+    potential_next_moves: Vec<ChessMove>,
+    parent_index: Option<usize>,
+    best_next: Option<usize>,
     alpha: i16,
     beta: i16,
     depth: u8,
 }
 
 impl Position {
-    pub fn new(chess_move: ChessMove, alpha: i16, beta: i16, depth: u8) -> Position {
+    pub fn new(
+        chess_move: Option<ChessMove>,
+        parent_index: Option<usize>,
+        alpha: i16,
+        beta: i16,
+        depth: u8,
+    ) -> Position {
         Position {
             chess_move: chess_move,
-            potential_next_moves: None,
+            potential_next_moves: vec![],
+            parent_index: parent_index,
+            best_next: None,
             alpha: alpha,
             beta: beta,
             depth: depth,
@@ -25,72 +34,90 @@ impl Position {
 }
 
 pub struct Search {
-    positions: Tree<Position>,
+    positions: Vec<Position>,
     color: Color,
     board: Board,
-    chess_move: ChessMove,
 }
 
 impl Search {
-    pub fn new(board: &Board, color: Color, chess_move: ChessMove) -> Search {
+    pub fn new(board: &Board, color: Color) -> Search {
         info!("Creating Search with color {:?}", board.side_to_move());
+        let mut position = Position::new(None, None, i16::MIN, i16::MAX, 0);
+        position.potential_next_moves = MoveGen::new_legal(board).collect();
+
         Search {
-            positions: tr(Position::new(chess_move, i16::MIN, i16::MAX, 0)),
+            positions: vec![position],
             color: color,
             board: board.clone(),
-            chess_move: chess_move,
         }
     }
 
     pub fn run(&mut self, max_depth: u8, alpha: Option<i16>, beta: Option<i16>) {
-        let mut root = self.positions.root_mut();
-        root.data_mut().alpha = alpha.unwrap_or(root.data_mut().alpha);
-        root.data_mut().beta = alpha.unwrap_or(root.data_mut().beta);
-
-        let mut moves = vec![root.data().chess_move];
-        let mut current = self.positions.root_mut();
+        let mut i = 0;
         loop {
-            if current.data().depth < max_depth {
-                if current.data().potential_next_moves.is_none() {
-                    let board = board_from_moves(self.board.clone(), &moves);
-                    let legal_moves = MoveGen::new_legal(&board).collect();
-                    current.data_mut().potential_next_moves = Some(legal_moves);
-                }
-                match &mut current.data_mut().potential_next_moves {
-                    Some(potential_moves) if potential_moves == &[] => {
-                        if current.has_no_child() {}
-                        // There are no more moves either because we used them all
-                        // or there vere none to begin with.
-                        //
-                        // If there are children, then the a/b are set correctly
-                        // and we can move up with the a/b
-                        //
-                        // If there were no potential moves, then we have to evaluate this position
-                    }
-                    Some(potential_moves) => {
-                        let next_move = potential_moves.pop().unwrap();
-                        let alpha = current.data().alpha;
-                        let beta = current.data().beta;
-                        let depth = current.data().depth + 1;
+            let mut current = &mut self.positions[i];
+            if current.depth < max_depth {
+                match current.potential_next_moves.pop() {
+                    Some(mv) => {
+                        let mut new_position = Position::new(
+                            Some(mv),
+                            Some(i),
+                            current.alpha,
+                            current.beta,
+                            current.depth + 1,
+                        );
+                        let new_board = &self.board_at_position(&new_position);
+                        match new_board.status() {
+                            BoardStatus::Ongoing => {
+                                new_position.potential_next_moves =
+                                    MoveGen::new_legal(new_board).collect();
+                            }
+                            _ => {
+                                // here we need to eval the board
+                            }
+                        }
 
-                        current.push_back(tr(Position::new(next_move, alpha, beta, depth)));
-                        current = current.back_mut().unwrap().back_mut().unwrap();
-
-                        moves.push(next_move);
+                        self.positions.push(new_position);
                     }
                     None => {
-                        panic!("this field should have been initialized already")
+                        // Go to parent and set A/B
+                        // if you're A or B "winns", then set best_next
                     }
                 }
+            } else {
+                // Go to parent and set A/B
+                // if you're A or B "winns", then set best_next
             }
         }
     }
-}
 
-fn board_from_moves(initial_board: Board, moves: &Vec<ChessMove>) -> Board {
-    let mut board = initial_board.clone();
-    for mv in moves.iter() {
-        board = board.make_move_new(*mv);
+    pub fn board_at_position(&self, position: &Position) -> Board {
+        let mut moves = vec![];
+        let mut current_position = position;
+        loop {
+            match current_position.chess_move {
+                Some(mv) => {
+                    moves.push(mv);
+                }
+                None => {
+                    break;
+                }
+            }
+
+            match current_position.parent_index {
+                Some(parent_index) => {
+                    current_position = &self.positions[parent_index];
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        let mut board = self.board.clone();
+
+        for mv in moves.iter().rev() {
+            board = board.make_move_new(*mv);
+        }
+        board
     }
-    board
 }
